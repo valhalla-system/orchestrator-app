8988f12a148f84d996762612f69772760a4093b8
Wojciech Janota <wojciech@janota.ovh>
2022-10-07 13:56:49 +0200

    Skeleton

---
+++ network/communication.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+class Server(host, port, name, access_password, version):
+    app = Flask(self.name)
+
+    @app.route("/")
+    def basic_server_data(self):
+        return {"server_name": self.name, "server_version": self.version, "host": self.host}
+
+    @app.route("/client/register")
+    def register_new_client_to_database(self):
+        # TODO: implement
